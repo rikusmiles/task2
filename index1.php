388dfbe47439467c96dc42e0d1e88d896096937f
Anushka Sinha <anushkasinha2696@gmail.com>
2016-09-16 18:29:56 +0530

    My third commit

---
+++ index1.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Project-Sign up page</title>
+	<link rel="stylesheet" type="text/css" href="Bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="Bootstrap/css/login-style.css">
+    <link rel="stylesheet" type="text/css" href="Bootstrap/css/animate.css">
+
+</head>
+
+<body>
+
+  <div class="container">
+    <div class="row">
+      <div class="jumbotron">
+    	  		<div class="form-group">
+    	  		 <center>
+    	  		 <h1>Sign up page</h1>
+    	  		 </centre>
+    	  	    </div>
+      </div>
+    	<div class="col-md-offset-4 col-sm-offset-4 col-xs-4 col-sm-4 col-md-4 col-lg-4">
+    	  	<form action="" method="POST" class="form-horizontal" role="form">
+    	  	
+    	  	<div class="form-group">
+    	  		<h2>Enter details</h2>
+    	  	</div>
+
+
+             <div class="form-group">
+            	<label class="control-label animated bounceInDown">Email</label><br>
+            	  <div class="col-sm-9">
+            		<input type="text" name="" id="input" class="form-control animated bounceInDown" value="" required="required" pattern="" title="" placeholder="Email id">
+            	  </div>
+              </div>
+
+             <div class="form-group">
+            	<label class="control-label animated bounceInDown">Password</label><br>
+            	 <div class="col-sm-9">
+            		<input type="text" name="" id="password" class="form-control animated bounceInDown" value="" required="required" pattern="" title="" placeholder="Password">
+            	 </div>
+             </div>
+
+             <div class="form-group">
+            	<label class="control-label animated bounceInDown">Confirm Password</label><br>
+            	 <div class="col-sm-9">
+            		<input type="text" name="" id="confirm_password" class="form-control animated bounceInDown" value="" required="required" pattern="" title="" placeholder="Confirm Password">
+            	 </div>
+             </div>
+
+             <div class="form-group">
+            	<label class="control-label animated bounceInDown">Username</label><br>
+            	 <div class="col-sm-9">
+            		<input type="text" name="" id="input" class="form-control animated bounceInDown" value="" required="required" pattern="" title="" placeholder="Choose username">
+            	 </div>
+             </div>
+
+    	  	 <div class="form-group">
+    	  		<div class="col-sm-9">
+    	  			<button type="submit" class="btn btn-primary">Submit</button>
+    	  		</div>
+    	  	 </div>
+
+    	    </form>
+        </div>
+    </div>
+  </div>
+</body>
+</html>
---
+++ index2.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Project Login Page</title>
+	<link rel="stylesheet" type="text/css" href="Bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="Bootstrap/css/login-style.css">
+    <link rel="stylesheet" type="text/css" href="Bootstrap/css/animate.css">
+
+</head>
+
+<body>
+
+  <div class="container">
+    <div class="row">
+      <div class="jumbotron">
+            <div class="form-group">
+             <center>
+             <h1>Sign up page</h1>
+             </centre>
+            </div>
+      </div>
+    	<div class="col-md-offset-4 col-sm-offset-4 col-xs-4 col-sm-4 col-md-4 col-lg-4">
+    	  	
+    	  	<form action="" method="POST" class="form-horizontal" role="form">
+
+                    <div class="form-group">
+            <h2>Enter details</h2>
+          </div>
+
+             <div class="form-group">
+            	<label class="control-label animated bounceInDown">Username</label><br>
+            	  <div class="col-sm-9">
+            		<input type="text" name="" id="input" class="form-control animated bounceInDown" value="" required="required" pattern="" title="" placeholder="Username">
+            	  </div>
+              </div>
+             <div class="form-group">
+            	<label class="control-label animated bounceInDown" style="animation-delay: 1s;">Password</label><br>
+            	 <div class="col-sm-9">
+            		<input type="text" name="" id="input" class="form-control animated bounceInDown" style="animation-delay: 1s;" value="" required="required" pattern="" title="" placeholder="Password">
+            	 </div>
+             </div>
+
+    	  	 <div class="form-group">
+    	  		<div class="col-sm-9">
+    	  			<button type="submit" class="btn btn-primary">Submit</button>
+    	  		</div>
+    	  	 </div>
+    	  	 <div class="form-group">
+    	  	 	<label class="control-label">Don't have an Account?</label>
+    	  	 	<a href="index1.php">Sign Up</a><br>
+    	  	 	<a href="">Forgot password?</a>
+    	  	 </div>
+    	    </form>
+        </div>
+    </div>
+  </div>
+</body>
+</html>
